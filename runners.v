bec81554bb1a4d3bc1606a3d062cd5d4baee3364
Joey Dodds <jdodds@galois.com>
2016-06-08 10:24:10 -0700

    initial commit

---
+++ runners.v
@@ -0,0 +1,23 @@
+Require Import Coq.Reals.Rdefinitions.
+Require Import Coq.Reals.Raxioms.
+Require Import Coq.Reals.Rfunctions.
+Require Import Coq.Lists.List.
+
+Local Open Scope R.
+
+Parameter floor : R -> R.
+
+Axiom floor_correct : forall R1 R2, floor R1 = R2 ->
+                              R2 <= R1 /\ exists (n : Z), (IZR n) = R2 /\ (forall n2, (IZR n2 < R1) -> (n2 <= n)%Z). 
+
+Definition get_positions (speeds : list nat) (time : R) :=
+  map (fun x=> floor (Rmult (INR x) time)) speeds.
+
+Definition is_lonely (i : nat) (positions : list R) : Prop :=
+  forall i2, i <> i2  -> (i < length positions)%nat -> (i2 < length positions)%nat ->
+        (R_dist (nth_default R0 positions i) (nth_default R0 positions i2) >= (Rdiv 1 (INR (length positions)))).
+
+Definition the_conjecture :=
+  forall (speeds : list nat) (runner : nat),
+    (runner < length speeds)%nat ->
+    exists (time : R), is_lonely runner (get_positions speeds time).
